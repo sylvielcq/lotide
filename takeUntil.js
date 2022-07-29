016b346d04878497588b742e575e31701fdbf779
sylvielcq <slcq.dev@gmail.com>
2022-07-28 19:52:28 -0700

    Created the takeUntil function

---
+++ takeUntil.js
@@ -0,0 +1,49 @@
+// ASSERTION FUNCTIONS
+// eqArrays function that compares 2 arrays.
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// assertArraysEqual function that asserts that 2 arrays are equal.
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} equal to ${arr2}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${arr1} not equal to ${arr2}`);
+  }
+};
+
+
+// ACTUAL FUNCTION
+
+const takeUntil = function(array, callback) {
+  let results = [];
+  for (let item of array) {
+    if (callback(item)) {
+      return results;
+    }
+    results.push(item);
+  }
+  return results;
+};
+
+
+// TEST CODE
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);

8ee61a23861c0538da9c48e223601fd458b83e5c
sylvielcq <slcq.dev@gmail.com>
2022-08-01 13:49:09 -0700

    Refactored middleTest with Mocha and Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,34 @@
-// Require the assertEqual function from its original file
-const assertArraysEqual = require('../assertArraysEqual');
-
 // Require the head function from its original file
 const middle = require('../middle');
 
+// TEST CODE USING MOCHA AND CHAI
+const assert = require('chai').assert;
 
-// TEST CODE FOR MIDDLE
+describe("#middle", () => {
+  it("returns an empty array for arrays with one element", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("returns an empty array for arrays with two elements", () => {
+    assert.deepEqual(middle([1, 2]), []); 
+  });
+  it("returns the single middle element for arrays with odd numbes", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  })
+  it("returns an array containing the two elements in the middle for arrays with an even number of elements", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+  })
+});
+
+
+
+
+
+/*
+// OLD TEST CODE FOR MIDDLE
+
+// Require the assertEqual function from its original file
+const assertArraysEqual = require('../assertArraysEqual');
+
 const test1 = middle([1]);
 assertArraysEqual(test1, []);
 
@@ -23,3 +46,4 @@ assertArraysEqual(test5, [2, 3]);
 
 const test6 = middle([1, 2, 3, 4, 5, 6]);
 assertArraysEqual(test6, [3, 4]);
+*/

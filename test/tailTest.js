8faa4f59562490bb326e93462ec30f9cbe3594c3
sylvielcq <slcq.dev@gmail.com>
2022-08-01 13:41:38 -0700

    Refactored tailTest with Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,30 @@
-// Require the assertEqual function from its original file
-const assertEqual = require('../assertEqual');
-
 // Require the head function from its original file
 const tail = require('../tail');
 
+// TEST CODE USING MOCHA AND CHAI
+const assert = require('chai').assert;
+
+describe("#tail", () => {
+  it("returns [2, 3, 4] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4]), [2, 3, 4]);
+  });
+  it("returns an empty array for [1]", () => {
+    assert.deepEqual(tail([1]), []); 
+  });
+  it("returns an empty array if given an empty array", () => {
+    assert.deepEqual(tail([]), []);
+  })
+});
 
 
-// TEST CODE FOR TAIL
+
+
+/*
+// OLD TEST CODE FOR TAIL
+
+// Require the assertEqual function from its original file
+const assertEqual = require('../assertEqual');
+
 console.log(tail([1, 2, 3, 4]));
 console.log(tail([1]));
 console.log(tail([]));
@@ -14,3 +32,4 @@ console.log(tail([]));
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
+*/
